8e035da7ee823d62942e0b78133df83d611067da
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-05-03 09:43:28 +0900

    Update 자릿수 더하기.java

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -5,8 +5,8 @@ public class Solution {
         int answer = 0;
 
         while (n > 0) {
-            answer += n % 10;
-            n /= 10;
+            answer += n % 10; // 10으로 나눈 나머지 더하기
+            n /= 10; // n을 10으로 나누면서 한 자리씩 소거
         }
 
         return answer;
