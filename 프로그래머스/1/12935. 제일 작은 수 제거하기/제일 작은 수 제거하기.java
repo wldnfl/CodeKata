1daeac9e90d12b4f4854dd9e99d4e8ddfb9c58c1
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-05-20 11:00:48 +0900

    Update 제일 작은 수 제거하기.java

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] solution(int[] arr) {
-        // 배열 길이가 1인 경우 -1로 반환하고 끝
+        // 배열 길이가 1인 경우 -1을 원소로 갖는 배열 반환
         if (arr.length == 1) {
             int[] answer_one = {-1};
             return answer_one;
@@ -9,15 +9,17 @@ class Solution {
         // 가장 작은 수 하나 빼야하니까 총 배열 길이에서 -1
         int[] answer = new int[arr.length - 1];
 
-        int min = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE; // min 변수 초기화
         for (int i = 0; i < arr.length; i++) {
             min = Math.min(min, arr[i]);
         }
-        int answer_num = 0;
+        
+        // 최소값을 제외한 값들로 새로운 배열 채우기
+        int answer_num = 0; // 새로운 배열 answer의 현재 인덱스 가리킴
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == min) {
-                continue;
-            } else {
+                continue; // 최소값 동일 원소 건너뛰기
+            } else { // 최소값 아닌 경우
                 answer[answer_num++] = arr[i];
             }
         }
