5674798973e355c6a2ba168804c82f5e71ac676f
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-05-22 09:52:02 +0900

    Update 내적.java

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int[] a, int[] b) {
         int answer = 0;
         for (int i = 0; i < a.length; i++) {
-            answer += a[i] * b[i];
+            answer += a[i] * b[i]; // a와 b 같은 인덱스끼리 곱해서 answer에 더해줌
         }
         return answer;
     }
