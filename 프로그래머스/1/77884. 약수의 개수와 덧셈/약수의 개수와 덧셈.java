abd1d0600488c991e4232f98f3bd8db329ada4e0
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-05-22 10:15:20 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.16 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for (int i = left; i <= right; i++) {
+            int cnt = 0; // 약수 개수 세기 위한 변수, 최소 1(자기 자신) 포함
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    cnt++; // j가 i의 약수면 cnt 증가
+                }
+            }
+            if (cnt % 2 == 0)
+                answer += i; // 짝수면 i를 answer에 더함
+            else
+                answer -= i; // 홀수면 뺌
+        }
+        return answer;
+    }
+}
