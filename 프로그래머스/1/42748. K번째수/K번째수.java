101b6e2f6b7252800e3712ac158f65d3ef9acdc5
Lee jiwoo <102974424+wldnfl@users.noreply.github.com>
2024-07-01 09:32:38 +0900

    [level 1] Title: K번째수, Time: 0.47 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] list = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+
+            Arrays.sort(list);
+
+            answer[i] = list[commands[i][2] - 1];
+        }
+        return answer;
+    }
+}
